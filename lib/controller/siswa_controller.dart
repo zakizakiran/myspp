7be5b84a859b6053e94298bca0f54463388e3e7b
zakizakiran <zzakiran4@gmail.com>
2023-03-27 20:30:34 +0700

    remove unused code

--- lib/controller/siswa_controller.dart
+++ lib/controller/siswa_controller.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:logger/logger.dart';
 import 'package:myspp_app/model/siswa.dart';
 import 'package:myspp_app/pages/siswa/data_siswa.dart';
 
